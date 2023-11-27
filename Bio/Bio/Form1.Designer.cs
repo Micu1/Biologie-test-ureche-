673064b8bf574ebc2e6a2330070a70d99503e153
Micu1 <151507514+Micu1@users.noreply.github.com>
2023-11-27 23:43:48 +0200

    Add files via upload

---
+++ Bio/Bio/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace Bio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PavUr = new System.Windows.Forms.TextBox();
+            this.CanAudExt = new System.Windows.Forms.TextBox();
+            this.Ciocan = new System.Windows.Forms.TextBox();
+            this.Nicov = new System.Windows.Forms.TextBox();
+            this.Timpan = new System.Windows.Forms.TextBox();
+            this.UrExt = new System.Windows.Forms.TextBox();
+            this.UrMed = new System.Windows.Forms.TextBox();
+            this.UrInt = new System.Windows.Forms.TextBox();
+            this.CavTim = new System.Windows.Forms.TextBox();
+            this.tromEust = new System.Windows.Forms.TextBox();
+            this.Coh = new System.Windows.Forms.TextBox();
+            this.FerRot = new System.Windows.Forms.TextBox();
+            this.NervCoh = new System.Windows.Forms.TextBox();
+            this.NervVest = new System.Windows.Forms.TextBox();
+            this.Vestibul = new System.Windows.Forms.TextBox();
+            this.Scarita = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // PavUr
+            // 
+            this.PavUr.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PavUr.Location = new System.Drawing.Point(32, 200);
+            this.PavUr.Multiline = true;
+            this.PavUr.Name = "PavUr";
+            this.PavUr.Size = new System.Drawing.Size(114, 56);
+            this.PavUr.TabIndex = 0;
+            // 
+            // CanAudExt
+            // 
+            this.CanAudExt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CanAudExt.Location = new System.Drawing.Point(71, 389);
+            this.CanAudExt.Multiline = true;
+            this.CanAudExt.Name = "CanAudExt";
+            this.CanAudExt.Size = new System.Drawing.Size(136, 56);
+            this.CanAudExt.TabIndex = 1;
+            // 
+            // Ciocan
+            // 
+            this.Ciocan.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Ciocan.Location = new System.Drawing.Point(439, 91);
+            this.Ciocan.Multiline = true;
+            this.Ciocan.Name = "Ciocan";
+            this.Ciocan.Size = new System.Drawing.Size(127, 27);
+            this.Ciocan.TabIndex = 2;
+            // 
+            // Nicov
+            // 
+            this.Nicov.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nicov.Location = new System.Drawing.Point(572, 90);
+            this.Nicov.Multiline = true;
+            this.Nicov.Name = "Nicov";
+            this.Nicov.Size = new System.Drawing.Size(126, 28);
+            this.Nicov.TabIndex = 3;
+            // 
+            // Timpan
+            // 
+            this.Timpan.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Timpan.Location = new System.Drawing.Point(439, 520);
+            this.Timpan.Multiline = true;
+            this.Timpan.Name = "Timpan";
+            this.Timpan.Size = new System.Drawing.Size(98, 33);
+            this.Timpan.TabIndex = 4;
+            // 
+            // UrExt
+            // 
+            this.UrExt.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UrExt.Location = new System.Drawing.Point(254, 611);
+            this.UrExt.Multiline = true;
+            this.UrExt.Name = "UrExt";
+            this.UrExt.Size = new System.Drawing.Size(153, 34);
+            this.UrExt.TabIndex = 5;
+            // 
+            // UrMed
+            // 
+            this.UrMed.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UrMed.Location = new System.Drawing.Point(543, 612);
+            this.UrMed.Multiline = true;
+            this.UrMed.Name = "UrMed";
+            this.UrMed.Size = new System.Drawing.Size(130, 33);
+            this.UrMed.TabIndex = 6;
+            // 
+            // UrInt
+            // 
+            this.UrInt.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UrInt.Location = new System.Drawing.Point(718, 609);
+            this.UrInt.Multiline = true;
+            this.UrInt.Name = "UrInt";
+            this.UrInt.Size = new System.Drawing.Size(143, 36);
+            this.UrInt.TabIndex = 7;
+            // 
+            // CavTim
+            // 
+            this.CavTim.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CavTim.Location = new System.Drawing.Point(595, 520);
+            this.CavTim.Multiline = true;
+            this.CavTim.Name = "CavTim";
+            this.CavTim.Size = new System.Drawing.Size(114, 56);
+            this.CavTim.TabIndex = 8;
+            // 
+            // tromEust
+            // 
+            this.tromEust.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tromEust.Location = new System.Drawing.Point(886, 484);
+            this.tromEust.Multiline = true;
+            this.tromEust.Name = "tromEust";
+            this.tromEust.Size = new System.Drawing.Size(114, 56);
+            this.tromEust.TabIndex = 9;
+            // 
+            // Coh
+            // 
+            this.Coh.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Coh.Location = new System.Drawing.Point(886, 418);
+            this.Coh.Multiline = true;
+            this.Coh.Name = "Coh";
+            this.Coh.Size = new System.Drawing.Size(114, 41);
+            this.Coh.TabIndex = 10;
+            // 
+            // FerRot
+            // 
+            this.FerRot.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FerRot.Location = new System.Drawing.Point(886, 352);
+            this.FerRot.Multiline = true;
+            this.FerRot.Name = "FerRot";
+            this.FerRot.Size = new System.Drawing.Size(186, 43);
+            this.FerRot.TabIndex = 11;
+            // 
+            // NervCoh
+            // 
+            this.NervCoh.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NervCoh.Location = new System.Drawing.Point(886, 286);
+            this.NervCoh.Multiline = true;
+            this.NervCoh.Name = "NervCoh";
+            this.NervCoh.Size = new System.Drawing.Size(148, 44);
+            this.NervCoh.TabIndex = 12;
+            // 
+            // NervVest
+            // 
+            this.NervVest.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NervVest.Location = new System.Drawing.Point(886, 231);
+            this.NervVest.Multiline = true;
+            this.NervVest.Name = "NervVest";
+            this.NervVest.Size = new System.Drawing.Size(186, 36);
+            this.NervVest.TabIndex = 13;
+            // 
+            // Vestibul
+            // 
+            this.Vestibul.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Vestibul.Location = new System.Drawing.Point(886, 170);
+            this.Vestibul.Multiline = true;
+            this.Vestibul.Name = "Vestibul";
+            this.Vestibul.Size = new System.Drawing.Size(136, 43);
+            this.Vestibul.TabIndex = 14;
+            // 
+            // Scarita
+            // 
+            this.Scarita.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Scarita.Location = new System.Drawing.Point(886, 76);
+            this.Scarita.Multiline = true;
+            this.Scarita.Name = "Scarita";
+            this.Scarita.Size = new System.Drawing.Size(202, 56);
+            this.Scarita.TabIndex = 15;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Lime;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(44, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 38);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Verifica!";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Lime;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(43, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(119, 37);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "Punctaj";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Lime;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(1021, 520);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(120, 39);
+            this.button3.TabIndex = 18;
+            this.button3.Text = "Reseteaza";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Bio.Properties.Resources.anatomia_urechii_768x474;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(1136, 648);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Scarita);
+            this.Controls.Add(this.Vestibul);
+            this.Controls.Add(this.NervVest);
+            this.Controls.Add(this.NervCoh);
+            this.Controls.Add(this.FerRot);
+            this.Controls.Add(this.Coh);
+            this.Controls.Add(this.tromEust);
+            this.Controls.Add(this.CavTim);
+            this.Controls.Add(this.UrInt);
+            this.Controls.Add(this.UrMed);
+            this.Controls.Add(this.UrExt);
+            this.Controls.Add(this.Timpan);
+            this.Controls.Add(this.Nicov);
+            this.Controls.Add(this.Ciocan);
+            this.Controls.Add(this.CanAudExt);
+            this.Controls.Add(this.PavUr);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox PavUr;
+        private System.Windows.Forms.TextBox CanAudExt;
+        private System.Windows.Forms.TextBox Ciocan;
+        private System.Windows.Forms.TextBox Nicov;
+        private System.Windows.Forms.TextBox Timpan;
+        private System.Windows.Forms.TextBox UrExt;
+        private System.Windows.Forms.TextBox UrMed;
+        private System.Windows.Forms.TextBox UrInt;
+        private System.Windows.Forms.TextBox CavTim;
+        private System.Windows.Forms.TextBox tromEust;
+        private System.Windows.Forms.TextBox Coh;
+        private System.Windows.Forms.TextBox FerRot;
+        private System.Windows.Forms.TextBox NervCoh;
+        private System.Windows.Forms.TextBox NervVest;
+        private System.Windows.Forms.TextBox Vestibul;
+        private System.Windows.Forms.TextBox Scarita;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Bio/Bio/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bio
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        int nr = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (PavUr.Text.ToLower()=="pavilionul urechii")
+            {
+                PavUr.BackColor = Color.Green;
+                nr++;
+                PavUr.ReadOnly = true;
+            }
+            else { PavUr.BackColor = Color.Red;
+                PavUr.ReadOnly= true;
+
+                 }
+            if(Ciocan.Text.ToLower() == "ciocan")
+            {
+                Ciocan.BackColor = Color.Green;
+                nr++;
+                Ciocan.ReadOnly = true;
+            }
+            else {Ciocan.BackColor = Color.Red;
+            Ciocan.ReadOnly= true;
+            }
+            if(CanAudExt.Text.ToLower() == "canalul auditiv extern")
+            {
+                CanAudExt.BackColor = Color.Green; nr++;
+                CanAudExt.ReadOnly = true;
+            }
+            else
+            { CanAudExt.BackColor = Color.Red;
+            CanAudExt.ReadOnly= true;
+            }
+            if(Nicov.Text.ToLower()== "nicovala")
+            {
+                Nicov.BackColor = Color.Green; nr++;
+                Nicov.ReadOnly = true;
+            }
+            else
+            { Nicov.BackColor = Color.Red;
+            Nicov.ReadOnly= true;
+            }
+            if(Scarita.Text.ToLower()== "scarita")
+            {
+                Scarita.BackColor = Color.Green;
+                Scarita.ReadOnly= true;
+                nr++;
+            }
+            else
+            { Scarita.BackColor = Color.Red;
+            Scarita.ReadOnly= true;
+            }
+            if(Vestibul.Text.ToLower()=="vestibul")
+            {
+                Vestibul.BackColor = Color.Green; nr++;
+                Vestibul.ReadOnly = true;
+            }
+            else
+            { Vestibul.BackColor= Color.Red;
+            Vestibul.ReadOnly= true;
+            }
+            if(NervVest.Text.ToLower()== "nervul vestibular")
+            {
+                NervVest.BackColor = Color.Green; nr++;
+                NervVest.ReadOnly= true;
+            }
+            else
+            { NervVest.BackColor= Color.Red;
+            NervVest.ReadOnly= true;
+            }
+            if(NervCoh.Text.ToLower() == "nervul cohlear")
+            {
+                NervCoh.BackColor = Color.Green; nr++;
+                NervCoh.ReadOnly= true;
+            }
+            else
+            { NervCoh.BackColor= Color.Red;
+            NervCoh.ReadOnly= true;
+            }
+            if(FerRot.Text.ToLower()== "fereastra rotunda")
+            {
+                FerRot.BackColor = Color.Green;
+                nr++;
+                FerRot.ReadOnly= true;
+            }
+            else
+            { FerRot.BackColor= Color.Red;
+            FerRot.ReadOnly= true;
+            }
+            if(Coh.Text.ToLower()== "cohlea")
+            {
+                Coh.BackColor = Color.Green;
+                nr++;
+                Coh.ReadOnly= true;
+            }
+            else { Coh.BackColor= Color.Red; Coh.ReadOnly= true; }
+            if(tromEust.Text == "trompa lui Eustachio")
+            {
+                tromEust.BackColor= Color.Green;
+                nr++;
+                tromEust.ReadOnly= true;
+            }
+            else
+            { tromEust.BackColor=Color.Red;
+            tromEust.ReadOnly= true;
+            }
+            if(CavTim.Text.ToLower()== "cavitatea timpanica")
+            {
+                CavTim.BackColor= Color.Green; nr++; 
+                    CavTim.ReadOnly= true;
+            }
+            else
+            { CavTim.ReadOnly = true;
+                CavTim.BackColor = Color.Red;
+            }
+            if (Timpan.Text.ToLower() == "timpan")
+            {
+                Timpan.BackColor = Color.Green; nr++;
+                Timpan.ReadOnly = true;
+            }
+            else
+            {
+                Timpan.BackColor = Color.Red;
+                Timpan.ReadOnly = true;
+            }
+            if(UrExt.Text.ToLower()== "urechea externa")
+            {
+                UrExt.BackColor= Color.Green;nr++;
+                UrExt.ReadOnly= true;
+            }
+            else
+            { UrExt.BackColor= Color.Red;
+            UrExt.ReadOnly= true;}
+            if(UrMed.Text.ToLower() == "urechea medie")
+            {
+                UrMed.BackColor= Color.Green;
+                nr++;
+                UrMed.ReadOnly= true;
+            }
+            else
+            { UrMed.BackColor= Color.Red;
+                UrMed.ReadOnly= true;
+            }
+            if(UrInt.Text.ToLower()== "urechea interna")
+            {
+                UrInt.BackColor= Color.Green;
+                nr++; UrInt.ReadOnly= true;
+            }
+            else
+            { UrInt.BackColor= Color.Red;
+                UrInt.ReadOnly= true;
+            }    
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (nr > 6)
+                MessageBox.Show("Felicitari ai obtinut " + nr + " din 16 puncte", "Afisare Punctaj!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            else MessageBox.Show("Din pacate ai picat acest test cu "+nr+" punct(e)","Esec",MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
